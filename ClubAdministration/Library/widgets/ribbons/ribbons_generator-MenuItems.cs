d11301d6ab02a5ef80af7b0e248ff0c51c9960ce
Javad Bayzavi <javadbayzavi@gmail.com>
2021-02-09 11:50:24 +0330

    menu item tests

--- ClubAdministration/Library/widgets/ribbons/ribbons_generator-MenuItems.cs
+++ ClubAdministration/Library/widgets/ribbons/ribbons_generator-MenuItems.cs
@@ -7,48 +7,127 @@ namespace ClubAdministration.Library.widgets.ribbons
 {
     public partial class ribbons_generator
     {
-        private static List<Models.ViewModels.ribbons> menus_details_default(params object[] rootParams)
+        private static List<Models.ViewModels.ribbons> menus_items_details(params object[] rootParams)
         {
-
             List<ClubAdministration.Models.ViewModels.ribbons> ribbons = new List<ClubAdministration.Models.ViewModels.ribbons>();
             ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
             {
-                action = "Edit",
+                action = "Edititem",
                 controller = "menus",
                 title = "ويرايش",
                 postback = false,
                 rootparam = rootParams[0],
                 selectionneeded = false,
                 hostform = "",
-                placeholder = "ويرايش منو",
+                placeholder = "ويرايش آيتم منو",
             });
             ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
             {
-                action = "Index",
+                action = "items",
                 controller = "menus",
                 title = "بازگشت",
                 postback = false,
-                rootparam = null,
+                rootparam = rootParams[0],
                 selectionneeded = false,
                 hostform = "",
                 placeholder = "بازگشت به صفحه قبل"
             });
             return ribbons;
         }
-        private static List<Models.ViewModels.ribbons> menus_delete_default(params object[] rootParams)
+        private static List<Models.ViewModels.ribbons> menus_items_delete(params object[] rootParams)
         {
 
             List<ClubAdministration.Models.ViewModels.ribbons> ribbons = new List<ClubAdministration.Models.ViewModels.ribbons>();
             ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
             {
-                action = "Delete",
+                action = "Deleteitem",
                 controller = "menus",
                 title = "حذف",
                 postback = true,
                 rootparam = rootParams[0],
                 selectionneeded = false,
                 hostform = "menusform",
-                placeholder = "حذف منو",
+                placeholder = "حذف الگوي تمرين",
+            });
+            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
+            {
+                action = "patterns",
+                controller = "menus",
+                title = "بازگشت",
+                postback = false,
+                rootparam = rootParams[1],
+                selectionneeded = false,
+                hostform = "",
+                placeholder = "بازگشت به صفحه قبل"
+            });
+            return ribbons;
+        }
+
+        private static List<Models.ViewModels.ribbons> menus_items_default(params object[] rootParams)
+        {
+
+            List<ClubAdministration.Models.ViewModels.ribbons> ribbons = new List<ClubAdministration.Models.ViewModels.ribbons>();
+            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
+            {
+                action = "Createitem",
+                controller = "menus",
+                title = "ايجاد",
+                postback = false,
+                rootparam = rootParams[0],
+                selectionneeded = false,
+                hostform = "",
+                placeholder = "ايجاد آيتم منو",
+            });
+            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
+            {
+                action = "Edititem",
+                controller = "menus",
+                title = "ویرایش",
+                placeholder = "ویرایش آيتم منو",
+                postback = false,
+                rootparam = null,
+                selectionneeded = true,
+            });
+            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
+            {
+                action = "Detailsitem",
+                controller = "menus",
+                title = "نمایش",
+                placeholder = "نمایش آيتم منو",
+                postback = false,
+                rootparam = null,
+                selectionneeded = true,
+            });
+            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
+            {
+                action = "Deleteitem",
+                controller = "menus",
+                title = "حذف",
+                placeholder = "حذف آيتم منو",
+                postback = false,
+                rootparam = null,
+                selectionneeded = true,
+            });
+            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
+            {
+                action = "Defaultitem",
+                controller = "menus",
+                title = "پيش‌فرض",
+                placeholder = "تعيين پيش فرض",
+                postback = false,
+                rootparam = rootParams[0],
+                selectionneeded = true,
+            });
+            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
+            {
+                action = "items",
+                controller = "menus",
+                title = "به‌روزآوري",
+                postback = false,
+                rootparam = rootParams[0],
+                selectionneeded = false,
+                hostform = "",
+                placeholder = "بازآوري مجدد ليست"
             });
             ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
             {
@@ -60,74 +139,6 @@ namespace ClubAdministration.Library.widgets.ribbons
                 selectionneeded = false,
                 hostform = "",
                 placeholder = "بازگشت به صفحه قبل"
-            });
-            return ribbons;
-        }
-        private static List<Models.ViewModels.ribbons> menus_list_default(params object[] rootParams)
-        {
-
-            List<ClubAdministration.Models.ViewModels.ribbons> ribbons = new List<ClubAdministration.Models.ViewModels.ribbons>();
-            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
-            {
-                action = "Create",
-                controller = "menus",
-                title = "ايجاد",
-                postback = false,
-                rootparam = null,
-                selectionneeded = false,
-                hostform = "",
-                placeholder = "ايجاد منو",
-            });
-            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
-            {
-                action = "Edit",
-                controller = "menus",
-                title = "ویرایش",
-                placeholder = "ویرایش اطلاعات منو",
-                postback = false,
-                rootparam = null,
-                selectionneeded = true,
-            });
-            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
-            {
-                action = "Details",
-                controller = "menus",
-                title = "نمایش",
-                placeholder = "نمایش اطلاعات منو",
-                postback = false,
-                rootparam = null,
-                selectionneeded = true,
-            });
-            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
-            {
-                action = "Delete",
-                controller = "menus",
-                title = "حذف",
-                placeholder = "حذف اطلاعات منو",
-                postback = false,
-                rootparam = null,
-                selectionneeded = true,
-            });
-            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
-            {
-                action = "items",
-                controller = "menus",
-                title = "آيتم‌ها",
-                placeholder = "آيتم‌هاي منو",
-                postback = false,
-                rootparam = rootParams[0],
-                selectionneeded = true,
-            });
-            ribbons.Add(new ClubAdministration.Models.ViewModels.ribbons()
-            {
-                action = "Index",
-                controller = "menus",
-                title = "به‌روزآوري",
-                postback = false,
-                rootparam = null,
-                selectionneeded = false,
-                hostform = "",
-                placeholder = "بازآوري مجدد ليست"
             });
             return ribbons;
         }
